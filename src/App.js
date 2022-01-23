d67f994e73471268547ac17521b50bf056cc8861
Viktor Hopkins <viktorhopkins00@gmail.com>
2022-01-24 00:01:44 +0100

    Expanding the About page

--- src/App.js
+++ src/App.js
@@ -1,43 +1,57 @@
-import { v4 as uuidv4 } from 'uuid'
-import {BrowserRouter as Router, Route} from 'react-router-dom'
-import { useState } from 'react'
+import { v4 as uuidv4 } from "uuid";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { useState } from "react";
 
-import Header from "./components/Header"
-import FeedbackList from "./components/FeedbackList"
-import FeedbackStats from './components/FeedbackStats'
-import FeedbackForm from './components/FeedbackForm'
-import AboutPage from './pages/AboutPage'
+import Header from "./components/Header";
+import FeedbackList from "./components/FeedbackList";
+import FeedbackStats from "./components/FeedbackStats";
+import FeedbackForm from "./components/FeedbackForm";
+import AboutPage from "./pages/AboutPage";
+import AboutIconLink from './components/AboutIconLink';
 
-import FeedbackData from './data/FeedbackData'
+import FeedbackData from "./data/FeedbackData";
 
 function App() {
-    const [feedback, setFeedback] = useState(FeedbackData)
+  const [feedback, setFeedback] = useState(FeedbackData);
 
-    const addFeedback = (newFeedback) => {
-        newFeedback.id = uuidv4()
-        setFeedback([newFeedback, ...feedback])
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4();
+    setFeedback([newFeedback, ...feedback]);
+  };
+
+  const deleteFeedback = (id) => {
+    if (window.confirm("Are you sure you want to delete?")) {
+      setFeedback(feedback.filter((item) => item.id !== id));
     }
-    
-    const deleteFeedback = (id) => {
-        if(window.confirm('Are you sure you want to delete?')){
-            setFeedback(feedback.filter((item) => item.id !== id))
-        }
-    }
+  };
 
-    return (
-        <Router>
-        <Header />
-        <div className='container'>
-            <Route exact path='/'>
-                <FeedbackForm handleAdd={addFeedback}/>
-                <FeedbackStats feedback={feedback}/>
-                <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
-            </Route>
+  return (
+    <Router>
+      <Header />
+      <div className="container">
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <>
+                <FeedbackForm handleAdd={addFeedback} />
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList
+                  feedback={feedback}
+                  handleDelete={deleteFeedback}
+                />
+              </>
+            }
+          ></Route>
 
-            <Route path='/s' component={AboutPage}/>
-        </div>
-        </Router>
-    )
+            <Route path='/about' element={<AboutPage />} />
+        </Routes>
+
+        <AboutIconLink />
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,17 +1,18 @@
+import {Link} from 'react-router-dom'
 import Card from '../components/shared/Card';
 
 function AboutPage() {
-  return (<Card>
+  return <Card>
       <div className='about'>
           <h1>About This Project</h1>
           <p>This is a React app to leave feedback for a product or service</p>
           <p>Version: 1.0.0</p>
           <p>
-              <a href='/'>Back To Home</a>
+              <Link to='/'>Back To Home</Link>
           </p>
 
       </div>
-  </Card>)
+  </Card>
 }
 
 export default AboutPage;

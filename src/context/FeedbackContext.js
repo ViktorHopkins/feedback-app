aeb3a321ab1f7fa95f474138a686f66b58afda11
Viktor Hopkins <viktorhopkins00@gmail.com>
2022-01-24 08:51:26 +0100

    Creating the Spinner component

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,32 +1,31 @@
 import { v4 as uuidv4 } from "uuid";
-import { createContext, useState } from "react";
-import { findAllByDisplayValue } from "@testing-library/react";
+import { createContext, useState, useEffect } from "react";
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState([
-    {
-      id: 1,
-      text: "This item is feedback item 1",
-      rating: 10,
-    },
-    {
-      id: 2,
-      text: "This item is feedback item 2",
-      rating: 9,
-    },
-    {
-      id: 3,
-      text: "This item is feedback item 3",
-      rating: 7,
-    },
-  ]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [feedback, setFeedback] = useState([]);
 
   const [feedbackEdit, setfeedbackEdit] = useState({
     item: {},
     edit: false,
   });
+
+  useEffect(() => {
+    fetchFeedback();
+  }, []);
+
+  // Fetch feedback
+  const fetchFeedback = async () => {
+    const response = await fetch(
+      "http://localhost:5000/feedback?_sort=id&_order=desc"
+    );
+    const data = await response.json();
+
+    setFeedback(data)
+    setIsLoading(false)
+  };
 
   // Add feedback
   const addFeedback = (newFeedback) => {
@@ -42,10 +41,10 @@ export const FeedbackProvider = ({ children }) => {
   };
 
   const updateFeedback = (id, updItem) => {
-      setFeedback(
-          feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item))
-      )
-  }
+    setFeedback(
+      feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item))
+    );
+  };
 
   // Set item to be update
   const editFeedback = (item) => {
@@ -60,6 +59,7 @@ export const FeedbackProvider = ({ children }) => {
       value={{
         feedback,
         feedbackEdit,
+        isLoading,
         deleteFeedback,
         addFeedback,
         editFeedback,

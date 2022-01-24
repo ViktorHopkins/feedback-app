a91f20c579f20351ac33434366e5e3e62b0674c3
Viktor Hopkins <viktorhopkins00@gmail.com>
2022-01-24 08:21:59 +0100

    Creating the Update feedback item

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -41,6 +41,13 @@ export const FeedbackProvider = ({ children }) => {
     }
   };
 
+  const updateFeedback = (id, updItem) => {
+      setFeedback(
+          feedback.map((item) => (item.id === id ? { ...item, ...
+            updItem } : item))
+      )
+  }
+
   // Set item to be update
   const editFeedback = (item) => {
     setfeedbackEdit({
@@ -53,10 +60,11 @@ export const FeedbackProvider = ({ children }) => {
     <FeedbackContext.Provider
       value={{
         feedback,
+        feedbackEdit,
         deleteFeedback,
         addFeedback,
         editFeedback,
-        feedbackEdit,
+        updateFeedback,
       }}
     >
       {children}

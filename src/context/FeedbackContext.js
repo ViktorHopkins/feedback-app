c7cfa266a64c82531f865e515449dec2c3b3e377
Viktor Hopkins <viktorhopkins00@gmail.com>
2022-01-24 09:17:43 +0100

    Delete and update from server

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -17,10 +17,8 @@ export const FeedbackProvider = ({ children }) => {
 
   // Fetch feedback
   const fetchFeedback = async () => {
-    const response = await fetch(
-      "/feedback?_sort=id&_order=desc"
-    )
-    const data = response.json();
+    const response = await fetch(`/feedback?_sort=id&_order=desc`)
+    const data = await response.json()
 
     setFeedback(data)
     setIsLoading(false)
@@ -42,25 +40,38 @@ export const FeedbackProvider = ({ children }) => {
   }
 
   // Delete feedback
-  const deleteFeedback = (id) => {
-    if (window.confirm("Are you sure you want to delete?")) {
-      setFeedback(feedback.filter((item) => item.id !== id));
+  const deleteFeedback = async (id) => {
+    if (window.confirm('Are you sure you want to delete?')) {
+      await fetch(`/feedback/${id}`, { method: 'DELETE' })
+
+      setFeedback(feedback.filter((item) => item.id !== id))
     }
-  };
+  }
 
-  const updateFeedback = (id, updItem) => {
+  // Update feedback item
+  const updateFeedback = async (id, updItem) => {
+    const response = await fetch(`/feedback/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(updItem),
+    })
+
+    const data = await response.json()
+
     setFeedback(
-      feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item))
-    );
-  };
+      feedback.map((item) => (item.id === id ? { ...item, ...data } : item))
+    )
+  }
 
-  // Set item to be update
+  // Set item to be updated
   const editFeedback = (item) => {
     setfeedbackEdit({
       item,
       edit: true,
-    });
-  };
+    })
+  }
 
   return (
     <FeedbackContext.Provider
